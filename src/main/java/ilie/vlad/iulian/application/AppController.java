cb9f9cb090269d25bcfd2672670e989bff11091e
ilie-vlad <vlad.iulian.ilie@gmail.com>
2018-03-14 19:18:22 +0200

    Refactor to match the functionality of serving xml file with xsl

--- src/main/java/ilie/vlad/iulian/application/AppController.java
+++ src/main/java/ilie/vlad/iulian/application/AppController.java
@@ -5,13 +5,11 @@ import ilie.vlad.iulian.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class AppController {
-
 	@Autowired
 	private AppService appService;
 
