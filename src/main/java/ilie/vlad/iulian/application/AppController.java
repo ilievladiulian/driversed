d1a2d9c1d3ad1e6554d80016cebfa42db7fd0926
ilie-vlad <vlad.iulian.ilie@gmail.com>
2018-03-14 22:05:54 +0200

    Solved template resolver error

--- src/main/java/ilie/vlad/iulian/application/AppController.java
+++ src/main/java/ilie/vlad/iulian/application/AppController.java
@@ -32,6 +32,6 @@ public class AppController {
 	@RequestMapping(value = "/parse", method = RequestMethod.GET)
 	public String parseXMLFile(@RequestParam(name = "file") String fileName, Model model) {
 		this.appService.parseXMLFile(fileName, model);
-		return "parsedFile";
+		return "uploadForm";
 	}
 }
